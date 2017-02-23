98e22b688397e9762b816119d56b501b580736b6
codlocker <ipsit.sahoo11@gmail.com>
2017-02-23 15:47:32 +0530

    Add Files for json data

---
+++ orderTrack/orders.js
@@ -0,0 +1,58 @@
+var orders = {
+  "0001":{
+    "status":"Pickup",
+    "executive_name":"XYZ BZZZ",
+    "number" : "90839223",
+    "pickup_date": "03-01-2009",
+    "pickup_time" : "10:00"
+  },
+  "0002":{
+    "status":"Repair",
+    "comments" : "The machine is beyond repair. Major parts change necessary",
+    "repair_date": "03-01-2009",
+    "repair_time" : "10:00"
+  },
+  "0003":{
+    "status":"Pickup",
+    "executive_name":"XYZ BZZZ",
+    "number" : "90839223",
+    "pickup_date": "03-01-2009",
+    "pickup_time" : "10:00"
+  },
+  "0004":{
+    "status":"Repair",
+    "comments" : "Repair Complete. Machine is running perfectly fine" ,
+    "repair_date": "03-01-2009",
+    "repair_time" : "10:00"
+  },
+  "0005":{
+    "status":"Pickup",
+    "executive_name":"XYZ BZZZ",
+    "number" : "90839223",
+    "pickup_date": "03-01-2009",
+    "pickup_time" : "10:00"
+  },
+  "0006":{
+    "status":"Delivery",
+    "executive_name":"XYZ BZZZ",
+    "number" : "90839223",
+    "Delivery_date": "03-01-2009",
+    "Delivery_time" : "10:00"
+  },
+  "0007":{
+    "status":"Delivery",
+    "executive_name":"XYZ BZZZ",
+    "number" : "90839223",
+    "Delivery_date": "03-01-2009",
+    "Delivery_time" : "10:00"
+  },
+  "0008":{
+    "status":"Delivery",
+    "executive_name":"XYZ BZZZ",
+    "number" : "90839223",
+    "Delivery_date": "03-01-2009",
+    "Delivery_time" : "10:00"
+  }
+};
+
+var data = JSON.stringify(orders);
